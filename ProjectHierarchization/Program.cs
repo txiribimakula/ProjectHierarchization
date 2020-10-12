25f2c2af8d9828b97ea73fdcb809c7c7cbb94ecf
Pablo Tobalina <pablotobalina@gmail.com>
2020-10-12 11:28:31 +0200

    Initial commit.

---
+++ ProjectHierarchization/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjectHierarchization
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
